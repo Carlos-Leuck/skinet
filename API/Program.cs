463e87e4737bdb5295c6e1566992d46637cf3b77
Carlos Leuck Moreira <carlinhosrlz@gmail.com>
2023-12-11 18:44:06 -0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,10 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(opt =>
@@ -19,7 +17,6 @@ builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 
 
 if (app.Environment.IsDevelopment())
@@ -27,7 +24,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
