5ea58fd0b375d524c7154eb885b08b3307dbdfc0
Carlos Leuck Moreira <carlinhosrlz@gmail.com>
2023-12-12 15:44:09 -0300

    Getting a single product with specification

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -33,7 +33,9 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetProduct(int id)
         {
-            return Ok(await _productRepo.GetByIdAsync(id));
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+
+            return Ok(await _productRepo.GetEntityWithSpec(spec));
         }
 
         [HttpGet("brands")]
